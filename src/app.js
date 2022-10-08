528fe3d8108952f160deb1450a04e0c832413475
lusferror <luisalfredorobles09@gmail.com>
2022-10-08 01:20:11 -0300

    code resuelto

--- src/app.js
+++ src/app.js
@@ -4,8 +4,71 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function cambioPinta(pinta, caso) {
+  var superior = document.getElementById("superior");
+  superior.innerHTML = pinta;
+  var inferior = document.getElementById("inferior");
+  inferior.innerHTML = pinta;
+  if (caso === 2 || caso === 3) {
+    superior.style.color = "red";
+    inferior.style.color = "red";
+  } else {
+    superior.style.color = "black";
+    inferior.style.color = "black";
+  }
+}
+function cambiCarta() {
+  document.getElementById("numero").innerHTML = Math.floor(
+    Math.random() * 12 + 1
+  );
+
+  let pintaRam = Math.floor(Math.random() * 3);
+  let pinta = "";
+  switch (pintaRam) {
+    case 1:
+      pinta = "♠";
+      cambioPinta(pinta, pintaRam);
+      break;
+    case 2:
+      pinta = "♥";
+      cambioPinta(pinta, pintaRam);
+      break;
+    case 3:
+      pinta = "♦";
+      cambioPinta(pinta, pintaRam);
+      break;
+    default:
+      pinta = "♣";
+      cambioPinta(pinta, pintaRam);
+      break;
+  }
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  cambiCarta();
+};
+
+document.getElementById("cambio").addEventListener("click", () => {
+  cambiCarta();
+});
+
+var myVar = window.setInterval(cambiCarta, 10000);
+
+window.selecionarCarta = function() {
+  clearInterval(myVar);
+  let pinta = document.getElementById("selectPinta");
+  let carta = document.getElementById("selectCard");
+  document.getElementById("numero").innerHTML = carta.value;
+  var superior = document.getElementById("superior");
+  superior.innerHTML = pinta.value;
+  var inferior = document.getElementById("inferior");
+  inferior.innerHTML = pinta.value;
+  if (pinta.value === "♥" || pinta.value === "♦") {
+    superior.style.color = "red";
+    inferior.style.color = "red";
+  } else {
+    superior.style.color = "black";
+    inferior.style.color = "black";
+  }
 };
